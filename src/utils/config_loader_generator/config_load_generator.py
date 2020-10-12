2b3bbc67d59104c1a906f1abfa5aea944cdbd618
Mutlu Polatcan <mutlupolatcan@gmail.com>
2020-10-12 11:39:38 +0300

    presto docker refactoring initial commit

---
+++ src/utils/config_loader_generator/config_load_generator.py
@@ -0,0 +1,63 @@
+# Written by Mutlu Polatcan
+# 21.10.2019
+
+import yaml
+import sys
+
+
+class Constants:
+    KEY_CONFIG_FILES = "config_files"
+    KEY_CONFIG_LOADER_SH_TEMPLATE = "config_loader_sh_template"
+    KEY_OUTPUT_DIR = "output_dir"
+    KEY_PATH = "path"
+    KEY_FILENAME = "filename"
+    CONFIG_LOADER_STD_STATEMENT_FMT = "load_config \"{property}\" ${{{env_var_name}:={env_var_value}}} \"{config_filename}\""
+    CONFIG_LOADER_SUBST_STATEMENT_FMT = "load_config \"{property}\" \"{substitution}\" \"{config_filename}\""
+
+
+class ConfigLoaderGenerator:
+    def __init__(self, config_filename):
+        self.__config = yaml.safe_load(open(config_filename, "r"))
+
+    def __get_infos(self, data, config_filename, prefix=None):
+        load_fn_calls = []
+
+        for property, value in data.items():
+            property_name = prefix + "." + property if prefix else property
+
+            if isinstance(value, dict):
+                load_fn_calls.extend(self.__get_infos(value, config_filename, property_name))
+            else:
+                property_name = property_name[:-1] if property_name[-1] == "." else property_name
+                env_var_name = property_name.upper().replace(".", "_").replace("-", "_")
+
+                if value.find("=") == 0:  # standard load config statement
+                    load_fn_calls.append(Constants.CONFIG_LOADER_SUBST_STATEMENT_FMT.format(
+                        property=property_name, substitution=value[1:], config_filename=config_filename)
+                    )
+                else:
+                    load_fn_calls.append(
+                        Constants.CONFIG_LOADER_STD_STATEMENT_FMT.format(property=property_name,
+                                                                         env_var_name=env_var_name,
+                                                                         env_var_value=value,
+                                                                         config_filename=config_filename)
+                    )
+
+        return load_fn_calls
+
+    def generate(self):
+        load_fn_calls = []
+
+        for config_info in self.__config[Constants.KEY_CONFIG_FILES]:
+            config_file_path, config_filename = config_info[Constants.KEY_PATH], config_info[Constants.KEY_FILENAME]
+
+            load_fn_calls.extend(self.__get_infos(yaml.safe_load(open(config_file_path, "r")), config_filename))
+            load_fn_calls.append("# ===========================================================================")
+
+        open("{loc}/config_loader.sh".format(loc=self.__config[Constants.KEY_OUTPUT_DIR]), "w").write(
+            self.__config[Constants.KEY_CONFIG_LOADER_SH_TEMPLATE].format(load_fn_calls="\n".join(load_fn_calls))
+        )
+
+
+if __name__ == "__main__":
+    ConfigLoaderGenerator(sys.argv[1]).generate()
