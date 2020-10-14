4373db1539532b3d3af4641c795cd058feeef4f8
Mutlu Polatcan <mutlupolatcan@gmail.com>
2020-10-15 01:16:57 +0300

    kubectl added to base image. leader elector script updates

--- src/base/presto_coordinator_leader_elector.sh
+++ src/base/presto_coordinator_leader_elector.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function is_leader() {
-    OTHER_PODS=$(for i in $COORDINATOR_REPLICA_COUNT; do echo presto-coordinator-$i; done | grep -v $HOSTNAME)
+    AVAILABLE_POD_COUNT=$(kubectl get pods | grep presto-coordinator | wc -l | awk '{print $1}')
+    OTHER_PODS=$(for i in $(seq 0 $(($AVAILABLE_POD_COUNT-1))); do echo presto-coordinator-$i; done | grep -v $HOSTNAME)
 
     for OTHER_POD in ${OTHER_PODS[@]}; do
         curl -I $OTHER_POD.presto-coordinator:${HTTP_SERVER_HTTP_PORT:=8080}
